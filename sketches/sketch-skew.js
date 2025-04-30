fc907dd2987e2415f4a3c1f653fcac918a18feae
Daniela Salazar <danii.salazar00@gmail.com>
2025-04-29 19:37:08 -0500

    Adding basic about skew a point

--- sketches/sketch-skew.js
+++ sketches/sketch-skew.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -6,6 +7,7 @@ const settings = {
 
 const sketch = () => {
   let x, y, w, h;
+  let radius, angle;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -18,7 +20,7 @@ const sketch = () => {
 
     context.save();
     context.translate(x, y);
-    context.translate(w * -.5, h * -.5);
+    // context.translate(w * -.5, h * -.5);
 
     context.strokeStyle = "blue";
     // context.strokeRect(x * -.5, y * -.5, w, h);
@@ -31,11 +33,20 @@ const sketch = () => {
     // context.lineTo(w * -.5, h * .5);
 
     // Rectangle with translate base
-    context.moveTo(0, 0);
-    context.lineTo(w, 0);
-    context.lineTo(w, h);
-    context.lineTo(0, h);
-    context.closePath();
+    // context.moveTo(0, 0);
+    // context.lineTo(w, 0);
+    // context.lineTo(w, h);
+    // context.lineTo(0, h);
+    // context.closePath();
+
+    radius = 200;
+    angle = math.degToRad(30);
+
+    x = Math.cos(angle) * radius;
+    y = Math.sin(angle) * radius;
+    
+    context.moveTo(0, 0)
+    context.lineTo(x, y);
 
     context.stroke();
 
